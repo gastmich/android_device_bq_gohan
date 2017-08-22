d29a3b1a6c60377e08ed2871a115612e8d9d0b38
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 13:38:19 -0700

    gohan: Add RenderScript HAL

--- device.mk
+++ device.mk
@@ -297,7 +297,8 @@ PRODUCT_PACKAGES += \
 	  android.hardware.audio.effect@2.0-impl \
 		android.hardware.broadcastradio@1.0-impl \
 		android.hardware.soundtrigger@2.0-impl \
-		android.hardware.memtrack@1.0-impl
+		android.hardware.memtrack@1.0-impl \
+    android.hardware.renderscript@1.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
