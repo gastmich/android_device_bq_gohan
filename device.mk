d42a71a56a9261444b9c172d9de65201fe84c7e4
mj1687 <alessandro.pasini12@gmail.com>
2018-08-21 21:33:18 +0200

    add some other hidls
    Signed-off-by: mj1687 <alessandro.pasini12@gmail.com>

--- device.mk
+++ device.mk
@@ -250,12 +250,22 @@ PRODUCT_COPY_FILES += \
 
 # HIDLs
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl
-    android.hardware.sensors@1.0-impl
+    android.hardware.light@2.0-impl \
+    android.hardware.sensors@1.0-impl \
 		camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl
-    android.hardware.gnss@1.0-impl
-    android.hardware.keymaster@3.0-impl
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.wifi@1.0-service  \
+		android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.biometrics.fingerprint@2.1-service \
+    android.hardware.power@1.0-impl \
+		android.hardware.audio@2.0-impl \
+	  android.hardware.audio.effect@2.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
