416b5e85505be55a478f39da89eafcbdbf5da37e
mj1687 <alessandro.pasini12@gmail.com>
2018-08-23 18:11:29 +0200

    add a prebuilt bootanimation.zip
    maybe you're asking why I'm doing this, well..I don't want to install ImageMagick so i've locally deleted the old bootanimation module,so sry lineage team
    
    Signed-off-by: mj1687 <alessandro.pasini12@gmail.com>

---
+++ bootanimation/Android.mk
@@ -0,0 +1,6 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE := bootanimation.zip
+LOCAL_SRC_FILES := $(LOCAL_MODULE).zip
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT)/media
+include $(BUILD_SYSTEM)/base_rules.mk
