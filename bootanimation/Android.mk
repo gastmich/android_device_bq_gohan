21ccc0728f18479c761c48be94d5c22746918a6d
Michael Gast <michl@tuxtronik.de>
2019-06-21 12:40:20 +0200

    delete bootanimation.zip

--- bootanimation/Android.mk
+++
@@ -1,6 +0,0 @@
-include $(CLEAR_VARS)
-LOCAL_MODULE := bootanimation.zip
-LOCAL_SRC_FILES := $(LOCAL_MODULE).zip
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/media
-include $(BUILD_SYSTEM)/base_rules.mk
